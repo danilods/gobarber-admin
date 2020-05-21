ecc8c4b0408304d727be6e83f81675a2a14b3733
Danilo de Sousa <danilod2.sousa@gmail.com>
2020-05-21 19:16:56 -0300

    GobarberAdmin: change confs

--- src/services/firebaseApi.ts
+++ src/services/firebaseApi.ts
@@ -3,12 +3,12 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
   const app = firebase.initializeApp({
-  apiKey: "AIzaSyA-e2hmJQP4D_37ZIMV1JxPzGR2D4O7kC8",
-    authDomain: "men-app-1fce4.firebaseapp.com",
-    databaseURL: "https://men-app-1fce4.firebaseio.com",
-    projectId: "men-app-1fce4",
-    storageBucket: "men-app-1fce4.appspot.com",
-    messagingSenderId: "1033048085356",
+  apiKey: "API_KEY",
+    authDomain: "xxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxxx.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "XXXXX.appspot.com",
+    messagingSenderId: "MESSAGE_ID",
 
 });
 
